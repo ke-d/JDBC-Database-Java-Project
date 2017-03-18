9ab3b00a7c1894bc6a8c42a1e05648c492350b03
mrdokenny <mrdokenny@gmail.com>
2017-03-17 20:07:33 -0700

    Added a line break between each loop run

--- 323 JDBC/src/CECS323JavaTermProject.java
+++ 323 JDBC/src/CECS323JavaTermProject.java
@@ -68,6 +68,7 @@ public class CECS323JavaTermProject {
                     
             }
             
+            System.out.println();
             sel = displayOptions();
         
         }
